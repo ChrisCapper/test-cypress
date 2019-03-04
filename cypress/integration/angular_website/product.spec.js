c69c22f64eff5518e3224b7ac4a4f949a7d39f79
student <student@safebear.co.uk>
2019-03-04 14:01:26 +0000

    comments added to code

--- cypress/integration/angular_website/product.spec.js
+++ cypress/integration/angular_website/product.spec.js
@@ -7,18 +7,23 @@ context('Product Tests (CRUD)', () => {
         // you can add extra queries to the url here too, eg cy.visit('/news/sport')
         cy.visit('')
     })
-
+    // name of specific test
     it("Creates a Product", () => {
+        // find the element 'a' and click it
         cy.get('a').click();
-
+        
+        // assert the url includes /product-add
         cy.url().should('include', '/product-add');
 
+        // find the element and type some text
         cy.get('#mat-input-0').type("Mizuno");
         cy.get('#mat-input-1').type("Kaboooom!");
         cy.get('#mat-input-2').type("300.00");
 
+        // find the button type submit and click it
         cy.get('[type="submit"]').click();
 
+        // find the element h2 and assert it contains some text
         cy.get('h2').should('contain', 'Mizuno');
 
     })
